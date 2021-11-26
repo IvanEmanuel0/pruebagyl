ccb53efb3cc549ee47e3b346e2c81998c86eabca
Marcos G. Frites <marcos.detti@hotmail.com>
2021-11-26 12:41:07 -0300

    Prueba de git - Creación de clase

---
+++ src/main/java/com/example/pruebagyl/Marcos.java
@@ -0,0 +1,14 @@
+package com.example.pruebagyl;
+
+import java.time.LocalDate;
+
+public class Marcos {
+
+    private Long dni;
+    private String nombre;
+    private String apellido;
+    private LocalDate fechaNacimiento;
+    private String email;
+    private String direccion;
+
+}
