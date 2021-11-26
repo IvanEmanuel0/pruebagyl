9e6d34cf6da1878e94af6f3ccecf9a74d2f045cd
Marcos G. Frites <marcos.detti@hotmail.com>
2021-11-26 12:52:44 -0300

    Modifico Flor

--- src/main/java/com/example/pruebagyl/Flor.java
+++ src/main/java/com/example/pruebagyl/Flor.java
@@ -3,4 +3,10 @@ package com.example.pruebagyl;
 public class Flor {
     private String nombre;
     private String apellido;
+    private Boolean preferida;
+
+    public Flor() {
+        this.preferida = true;
+    }
+
 }
