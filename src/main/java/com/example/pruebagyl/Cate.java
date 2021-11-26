ad8f07cc0047cc1ca919f741c1ffb84c91286916
Caterina Cunsolo <caterinacunsolo0@gmail.com>
2021-11-26 17:27:44 -0300

    jeje

--- src/main/java/com/example/pruebagyl/Cate.java
+++ src/main/java/com/example/pruebagyl/Cate.java
@@ -3,5 +3,6 @@ package com.example.pruebagyl;
 public class Cate {
     private String signo;
     private String name = "Ponchito del Rey";
+    private String pasion="BOKEEEEEEEEEEEEEEEEEEEEEEEEE";
     private Integer edad;
 }
