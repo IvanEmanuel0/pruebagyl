cd36a97860985a541652dfe8957ef5cea27a9f18
Iván Alagastino <alagastino.ivane@gmail.com>
2021-11-26 12:34:53 -0300

    primer prueba

---
+++ src/main/java/com/example/pruebagyl/PruebagylApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebagyl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebagylApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebagylApplication.class, args);
+	}
+
+}
