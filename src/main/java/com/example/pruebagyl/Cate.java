556af2c1adf2bc626a0d0d74d1027ee69759c552
Marcos G. Frites <marcos.detti@hotmail.com>
2021-11-26 17:38:19 -0300

    Modificacion nombre Cate - Merge

--- src/main/java/com/example/pruebagyl/Cate.java
+++ src/main/java/com/example/pruebagyl/Cate.java
@@ -2,7 +2,7 @@ package com.example.pruebagyl;
 
 public class Cate {
     private String signo;
-    private String name = "Ponchito del Rey";
+    private String name = "Cati Kitty";
     private String pasion="BOKEEEEEEEEEEEEEEEEEEEEEEEEE";
     private Integer edad=12;
 }
