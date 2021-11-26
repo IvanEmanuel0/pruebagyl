b6be147d9570cca850698970e2c500279aec75ac
Iván Alagastino <alagastino.ivane@gmail.com>
2021-11-26 12:40:46 -0300

    primer commit con intellij

---
+++ src/main/java/com/example/pruebagyl/Ivo.java
@@ -0,0 +1,31 @@
+package com.example.pruebagyl;
+
+public class Ivo {
+
+    String nombre;
+    String apellido;
+
+    public Ivo() {
+    }
+
+    public Ivo(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
